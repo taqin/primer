bc942cad39d5f557adc94d82bce8f7b9c3781c3b
Taqin <taqin@megentics.com>
2014-07-18 12:00:16 +0800

    ReInstall LarGen

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit79e5972cd30cc2dffd392353c6dddc2a::getLoader();
+return ComposerAutoloaderInit4137c5f8662b4cfaef1ba1fe67e1c774::getLoader();
